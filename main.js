49e7c16a345fb649819ccc7c2477cf0686c88dab
Ignacio Cuello <ignacio.cuello01@gmail.com>
2025-03-29 15:32:43 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function showSidebar(){
+    const sidenavbar = document.querySelector('.side-navbar');
+    sidenavbar.style.display='flex'
+
+}
+
+function closeSidebar(){
+    const sidenavbar = document.querySelector('.side-navbar');
+    sidenavbar.style.display= 'none'
+}
